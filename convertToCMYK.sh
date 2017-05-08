a5c69386c991d509a98af20fa6095f5801113b34
V.Syropoulos <vsyropou@protonmail.com>
2017-05-08 18:32:39 +0200

    Preparing first test print.

--- convertToCMYK.sh
+++ convertToCMYK.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-gs -o "${2}" -sDEVICE=pdfwrite -sProcessColorModel=DeviceCMYK -sColorConversionStrategy=CMYK -sColorConversionStrategyForImages=CMYK "${1}"
+gs -o "${2}" -sDEVICE=pdfwrite -sProcessColorModel=DeviceCMYK -sColorConversionStrategy=CMYK -sColorConversionStrategyForImages=CMYK -dAutoRotatePages=/None "${1}"
