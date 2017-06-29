45c48e95c460e4a0174105de6eef122c86d33261
V.Syropoulos <vsyropou@protonmail.com>
2017-06-29 13:10:47 +0200

    Fixed margins and corretly embed fonts

--- convertToCMYK.sh
+++ convertToCMYK.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-gs -o "${2}" -sDEVICE=pdfwrite -sProcessColorModel=DeviceCMYK -sColorConversionStrategy=CMYK -sColorConversionStrategyForImages=CMYK -dAutoRotatePages=/None "${1}"
+gs -o "${2}" -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sProcessColorModel=DeviceCMYK -sColorConversionStrategy=CMYK -sColorConversionStrategyForImages=CMYK -dAutoRotatePages=/None -dPDFSETTINGS=/prepress -dEmbedAllFonts=true "${1}"
